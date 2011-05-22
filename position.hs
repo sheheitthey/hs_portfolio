bb290620963b1b1b4fca8834b4dcd8134faf2899
Kenny To <sheheitthey@gmail.com>
2011-05-21 19:15:24 -0700

    Add Position and LongPosition.
    Position is a type class representing any type of position. The things that can
    be done with a position are to find its value, find its open cost, and find its
    closing cost.
    
    LongPosition is an instance of Position representing a long position (though I
    think I don't quite have the terminology right). That is, a position of
    directly owning shares of a security.
    
    The LongPosition data type is constructed with a number of shares, open cost,
    and close cost. Irritatingly, Haskell doesn't have any good support for a
    record type. I manually write my own field accessors instead of letting
    Haskell generate ones which can conflict with other generated field accessors.

--- position.hs
+++ position.hs
@@ -1,1 +1,28 @@
 -- Calculating positions.
+
+class Position a where
+    -- positionValue(Position p, Float securityValue)
+    positionValue :: a -> Float -> Float
+
+    -- positionOpenCost(Position p)
+    positionOpenCost :: a -> Float
+
+    -- positionCloseCost(Position p)
+    positionCloseCost :: a -> Float
+
+data LongPosition = LongPosition Int Float Float deriving (Show)
+
+longPosition_numShares :: LongPosition -> Int
+longPosition_numShares (LongPosition numShares _ _) = numShares
+
+longPosition_openCost :: LongPosition -> Float
+longPosition_openCost (LongPosition _ openCost _) = openCost
+
+longPosition_closeCost :: LongPosition -> Float
+longPosition_closeCost (LongPosition _ _ closeCost) = closeCost
+
+instance Position LongPosition where
+    positionValue p securityValue =
+        (fromIntegral (longPosition_numShares p)) * securityValue
+    positionOpenCost p = longPosition_openCost p
+    positionCloseCost p = longPosition_closeCost p
