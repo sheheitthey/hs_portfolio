51cb96f67d74d8d65ffcc4c3c495e63580ffce3d
Kenny To <sheheitthey@gmail.com>
2011-06-16 23:13:40 -0700

    Add basic Position functions.
    These are positionOpenCost, positionCloseCost, and positionValue.

--- Position.hs
+++ Position.hs
@@ -11,3 +11,40 @@ data Position =   SharePosition Int     -- numShares
                                  Float  -- openCost
                                  Float  -- closeCost
                 deriving (Show)
+
+positionOpenCost :: Position -> Float
+positionOpenCost position = case position of
+    (SharePosition numShares
+                   openCost
+                   closeCost) ->
+        openCost
+    (OptionPosition numShares
+                    strikePrice
+                    openCost
+                    closeCost) ->
+        openCost
+
+positionCloseCost :: Position -> Float
+positionCloseCost position = case position of
+    (SharePosition numShares
+                   openCost
+                   closeCost) ->
+        closeCost
+    (OptionPosition numShares
+                    strikePrice
+                    openCost
+                    closeCost) ->
+        closeCost
+
+positionValue :: Position -> Float -> Float
+positionValue position securityValue = case position of
+    (SharePosition numShares
+                   openCost
+                   closeCost) ->
+        (fromIntegral numShares) * securityValue
+    (OptionPosition numShares
+                    strikePrice
+                    openCost
+                    closeCost) ->
+        max ((fromIntegral numShares) * (securityValue - strikePrice))
+            0
